1e49cc129f6e13d7f0b9b0b3913bf9ea33e29270
jayanthan-prog <jayanthan212@gmail.com>
2025-03-26 17:34:13 +0530

    added crime map page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider, useAuth } from './context/AuthContext';
 import Layout from './components/Layout';
 import Login from './pages/Login';
+ import CrimeMap from './pages/CrimeMap';
+ import Analytics from './pages/Analytics';
  
 
 const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
